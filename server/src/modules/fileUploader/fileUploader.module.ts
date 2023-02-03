1cbc20fcd707553064e489b4d7f74619f61fc490
idopere1 <idoperez321@gmail.com>
2023-02-03 16:33:55 +0200

    remove

--- server/src/modules/fileUploader/fileUploader.module.ts
+++ server/src/modules/fileUploader/fileUploader.module.ts
@@ -1,10 +1,9 @@
 import { Module } from '@nestjs/common';
-import { TypeOrmModule } from '@nestjs/typeorm';
 import { UploaderFile } from './modules/UploaderFile.module';
 import { FileUploaderService } from './fileUploader.service';
 
 @Module({
-    imports: [TypeOrmModule.forFeature([UploaderFile])],
+    imports: [UploaderFile],
     providers: [FileUploaderService,],
     exports: [FileUploaderService],
 })
--- server/src/modules/fileUploader/fileUploader.service.ts
+++ server/src/modules/fileUploader/fileUploader.service.ts
@@ -1,62 +1,61 @@
-import { Injectable } from '@nestjs/common';
-import { ConfigService } from '@nestjs/config';
-import { InjectRepository } from '@nestjs/typeorm';
-import { S3 } from 'aws-sdk';
-import { env } from 'process';
-import { UploaderFile } from './modules/UploaderFile.module';
-import { Repository } from 'typeorm';
-import { v4 as uuid } from 'uuid';
-
+import { Injectable } from "@nestjs/common";
+import { ConfigService } from "@nestjs/config";
+import { InjectRepository } from "@nestjs/typeorm";
+import { S3 } from "aws-sdk";
+import { env } from "process";
+import { UploaderFile } from "./modules/UploaderFile.module";
+import { Repository } from "typeorm";
+import { v4 as uuid } from "uuid";
 
 @Injectable()
 export class FileUploaderService {
-    constructor(
-        @InjectRepository(UploaderFile)
-        private publicFilesRepository: Repository<UploaderFile>,
-        private readonly configService: ConfigService
-    ) { }
+  constructor() {}
 
-    async uploadFile(dataBuffer: Buffer, filename: string): Promise<string> {
-        const s3 = new S3();
-        const uploadResult = await s3.upload({
-            Bucket: env.AWS_PUBLIC_BUCKET_NAME,
-            ContentDisposition: 'inline',
-            ContentType: 'application/pdf',
-            Body: dataBuffer,
-            Key: `${uuid()}-${filename}`
-        }).promise();
+  async uploadFile(dataBuffer: Buffer, filename: string): Promise<string> {
+    const s3 = new S3();
+    const { Location: location } = await s3
+      .upload({
+        Bucket: env.AWS_PUBLIC_BUCKET_NAME,
+        ContentDisposition: "inline",
+        ContentType: "application/pdf",
+        Body: dataBuffer,
+        Key: `${uuid()}-${filename}`,
+      })
+      .promise();
 
-        const newFile = this.publicFilesRepository.create({
-            key: uploadResult.Key,
-            url: uploadResult.Location
-        });
-        await this.publicFilesRepository.save(newFile);
-        return newFile.url;
-    }
+    return location;
+  }
 
-    async uploadSingleFile(s3: S3, dataBuffer: Buffer, filename: string): Promise<string> {
-        const uploadResult = await s3.upload({
-            Bucket: env.AWS_PUBLIC_BUCKET_NAME,
-            Body: dataBuffer,
-            Key: `${uuid()}-${filename}`
-        }).promise();
-        const newFile = this.publicFilesRepository.create({
-            key: uploadResult.Key,
-            url: uploadResult.Location
-        });
+  async uploadSingleFile(
+    s3: S3,
+    dataBuffer: Buffer,
+    filename: string
+  ): Promise<string> {
+    const { Location: location } = await s3
+      .upload({
+        Bucket: env.AWS_PUBLIC_BUCKET_NAME,
+        Body: dataBuffer,
+        Key: `${uuid()}-${filename}`,
+      })
+      .promise();
 
-        await this.publicFilesRepository.save(newFile);
-        return newFile.url;
-    }
+    return location;
+  }
 
-    async uploadMultipleFiles(files: Array<Express.Multer.File>) {
-        const s3 = new S3();
-        let uploadFiles: string[] = [];
-        await Promise.all(files.map(async (file) => {
-            const currentUrl = await this.uploadSingleFile(s3, file.buffer, file.originalname);
-            uploadFiles.push(currentUrl);
-        }));
+  async uploadMultipleFiles(files: Array<Express.Multer.File>) {
+    const s3 = new S3();
+    let uploadFiles: string[] = [];
+    await Promise.all(
+      files.map(async (file) => {
+        const currentUrl = await this.uploadSingleFile(
+          s3,
+          file.buffer,
+          file.originalname
+        );
+        uploadFiles.push(currentUrl);
+      })
+    );
 
-        return uploadFiles;
-    }
+    return uploadFiles;
+  }
 }
